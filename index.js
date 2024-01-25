84dc28cec837d73c9c73ca49bdcf33d4813b861d
Your Name <m.karaarslan06@outlook.com>
2024-01-25 13:47:31 +0300

    router-usage

--- index.js
+++ index.js
@@ -11,25 +11,26 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 //? "Router" is special app for URL control in ExpressJS.
+// app.get("/", (req, res) => {
+//   res.send({ message: "GET request to the homepage" });
+// });
+// app.get("/about", (req, res) => {
+//   res.send({ message: "GET request to the about page" });
+// });
+// app.get("/user/:userId", (req, res) => {
+//   res.send({ message: "GET request to the user page" });
+// });
 
-// app.get('/', (req, res) => { res.send({ message: 'Home Page' }) })
-// app.get('/about', (req, res) => { res.send({ message: 'About Page' }) })
-// app.get('/user/:userId', (req, res) => { res.send({ message: 'User Page' }) })
-
-// Send routing to Router:
-// const router = express.Router()
-// router.get('/', (req, res) => { res.send({ message: 'Home Page' }) })
-// router.get('/about', (req, res) => { res.send({ message: 'About Page' }) })
-// router.get('/user/:userId', (req, res) => { res.send({ message: 'User Page' }) })
-// Router to App:
-// app.use(router)
-
-// const router = require('./routes/')
-// app.use(router)
-
-// app.use(require('./routes/'))
-
-app.use('/user', require('./routes/user'))
-
+const router = express.Router()
+router.get("/", (req, res) => {
+  res.send({ message: "GET request to the homepage" });
+});
+router.get("/about", (req, res) => {
+  res.send({ message: "GET request to the about page" });
+});
+router.get("/user/:userId", (req, res) => {
+  res.send({ message: "GET request to the user page" });
+});
+app.use(router)
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
